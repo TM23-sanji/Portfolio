2ecee97040ea0f5157d42e072da1a207e26683d9
TM23-sanji <tusharmishra802@gmail.com>
2025-05-08 16:07:29 +0530

    Test

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/Portfolio/', // <- add this
+  build: {
+    outDir: 'dist',
+  },
   server: {
     host: "::",
     port: 8080,
